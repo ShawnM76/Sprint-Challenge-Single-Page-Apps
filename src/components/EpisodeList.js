7a2dcc7ed8ece9382be49e28a582f8ad5cf17438
Shawn McManus <xsvfan76@gmail.com>
2019-07-26 10:45:42 -0600

    added my 3 axios call components and nav bar routes

---
+++ src/components/EpisodeList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+export default function EpisodeList() {
+    // TODO: Add useState to track data from useEffect
+    const [episode, setEpisode] = useState([]);
+  
+    useEffect(() => {
+      
+      // TODO: Add AJAX/API Request here - must run in `useEffect`
+      Axios 
+        .get(`https://rickandmortyapi.com/api/episode/`)
+        .then(res => {
+          const infoEpisode = res.data.results;
+          setEpisode(res.data.results);
+          console.log("Episode info", infoEpisode)
+        })
+        .catch(error => {
+          console.log('Server Error with Episode', error);
+        });
+        
+      //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    }, [])
+  
+    
+    return <section className='character-list grid-view'>
+  
+        <h2>This is Episodes!</h2>
+      </section>
+  
+  }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
 
 export default function LocationsList() {
+    const [location, setLocation] = useState([])
 
+    useEffect(() => {
+        Axios
+            .get('https://rickandmortyapi.com/api/location/')
+            .then(res => {
+                const infoLocation = res.data.results;
+                setLocation(res.data.results);
+                console.log("location Info", infoLocation)
+            })
+            .catch(error => {
+                console.log('Server Error with Location', error)
+            });
+        
+    }, [])
+
+    return <section className='character-list grid-view'>
+        <h2>This is location</h2>
+    </section>
 }
